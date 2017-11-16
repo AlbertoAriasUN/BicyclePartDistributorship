8bb48db6c27f3473abb0721ec3406c734e209282
Matthew Aneiro <matthewaneiro@gmail.com>
2017-11-16 16:00:01 -0500

    Added Warehouse Manager view, Warehouse Manager account

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,13 +10,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * Entery Point for Application/FXML
+ * Entry Point for Application/FXML
  * @author Matthew
  */
 public class Main extends Application {
 
 	private static Stage stage;
 
+	public static final String WAREHOUSE_MANAGER_STAGE_URL = "Views/WarehouseManager.fxml";
+	public static final String OFFICE_MANAGER_STAGE_URL = "Views/OfficeManager.fxml";
 	public static final String SYSADMIN_STAGE_URL = "Views/SysAdmin.fxml";
 	public static final String LOGIN_STAGE_URL = "Views/Login.fxml";
 	
@@ -43,7 +45,6 @@ public class Main extends Application {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-    	//APICaller.getUserController().registerUser("John", "Smith", "jsmith", "password", "jsmith@mail.umw.edu", UserType.OFFICE_MANAGER);
     	launch(args);
     }
 
